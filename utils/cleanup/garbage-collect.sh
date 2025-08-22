c990f44fc0acaab171419e61bc6c1e3e61a04430
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-08-21 22:11:09 -0400

    chore: do not run docker cleanup

--- utils/cleanup/garbage-collect.sh
+++ utils/cleanup/garbage-collect.sh
@@ -9,7 +9,7 @@ export REGISTRY_NAMESPACES=${PROJECT_NAMESPACES}
 
 bash ./cluster/k8s-orphaned-replicasets.sh;
 bash ./registry/registry-unreferenced-blobs.sh;
-bash ./docker/prune-containers-and-images.sh;
+# bash ./docker/prune-containers-and-images.sh;
 # bash ./cluster/kind/nodes-images-cache-prune.sh;
 bash ./cluster/k3s/nodes-images-cache-prune.sh;
 
